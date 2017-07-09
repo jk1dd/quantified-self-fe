14d7b8fa4affec454afa378cad6f7a059da51afd
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-09 16:01:14 -0600

    add functionality for diary tables
    populuates tables
    calculates total calories
    calculates remaining calories
    [Finishes #148036099]

---
+++ test/index-test.js
@@ -0,0 +1,40 @@
+var assert    = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var until = webdriver.until;
+var test      = require('selenium-webdriver/testing');
+var frontEndLocation = "http://localhost:8080"
+
+test.describe('testing food index', function() {
+  var driver;
+  this.timeout(1000000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+  })
+
+  test.afterEach(function() {
+    driver.quit();
+  })
+
+  test.it("lists four meal tables", function() {
+    driver.get(`${frontEndLocation}`)
+    driver.wait(until.elementLocated({css: "#meal-4 .remaining-calories"}))
+    driver.findElements({css: "body .meal table"})
+    .then(function(meals) {
+      assert.lengthOf(meals, 4);
+    })
+  })
+  
+  test.it("lists four meal tables", function() {
+    driver.get(`${frontEndLocation}`)
+    driver.wait(until.elementLocated({css: "#meal-4 .remaining-calories"}))
+    driver.findElements({css: "body .meal table"})
+    .then(function(meals) {
+      assert.lengthOf(meals, 4);
+    })
+  })
+
+
+});
